eea48c19bb5dfa8e2a9ae170313f9f1c975e070d
isabelangelo <isabelangelo@berkeley.edu>
2016-11-16 22:27:05 -0800

    added text function, made Events class

--- bl_update_gbt_schedule.py
+++ bl_update_gbt_schedule.py
@@ -2,118 +2,74 @@ from quickstart import *
 from gbt_scraper import *
 import time
 import copy
+import sys
+from twilio.rest import TwilioRestClient
 
-'''
-	Written by Isabel Angelo, UCB,  Fall 2016
-	Modified by Howard Isaacson, UCB, Fall 2016
-	
-	Purpose:  Automatically update the GBT calendar 
-	
-	Resources:
-		Website describing google calendar API.
-		https://developers.google.com/google-apps/calendar/quickstart/python
-		Obtain .json file as described in above link.
-		Change SCOPE from readonly to read/write
-		
-		Install:
-		pip install apiclient
-		pip install --upgrade google-api-python-client
+sys.path.insert(0, '/Users/isabelangelo/Desktop') #path to where credentials are stored
+from googleAPI_info import *
 
-	calendar:    u'summary': u'BL_Obs'		
-'''
+def start_time(scraped_event):
+	''' gets start time of an event scraped GBT events list'''
+	e = scraped_event.start[:10]+'T'+event.start[11:]+':00Z'
+	e = e.decode('unicode_escape')
+	return e
 
-#get credentials and calendar list from Google Calendar
-credentials = get_credentials()
-http = credentials.authorize(httplib2.Http())
-service = discovery.build('calendar', 'v3', http=http)
-page_token = None
-BL_CAL = 'BL_Obs'  #Choose to update calendar with this name
-while True:
-  calendar_list = service.calendarList().list(pageToken=page_token).execute()
-  for calendar_list_entry in calendar_list['items']: #hti
-    print 'calendar name:',calendar_list_entry['summary'] #hti
-    if calendar_list_entry['summary'] == BL_CAL:
-      cal = calendar_list_entry# ['items']
-      print 'credentials successful, calendar retrieved'
-      break
-  page_token = calendar_list.get('nextPageToken')
-  if not page_token:
-    break
+def end_time(scraped_event):
+	''' gets start time of an event scraped GBT events list '''
+	e = scraped_event.end[:10]+'T'+event.end[11:]+':00Z'
+	e = e.decode('unicode_escape')
+	return e
 
-#stores which google calendar to update
-cal = calendar_list['items'][2] #1=index of BL google calendar in calendar list
-#hti commented ths out.
+class Event:
+	'''Class for storing information about existing Google Calendar Events'''
+	def __init__(self, summary= None, start=None, end=None, link=None):
+		self.summary = summary
+		self.start = start
+		self.end = end
+		self.link = link
 
-#alternately set this up to find item in calendar_list called 'BL_Obs'
-
-#scrape and store schedule from GBT website
-scraper = GBTScraper(date=time.strftime("%m/%d/%Y"), days = 30) #store for next 30 days
-scraper.make(t = 'utc')
-#HTI combine events if end time of one is start time of next.
-#if len(scraper.btlpSchedule) > 1:
-#	for session in scraper.btlpSchedule:
-#		session1 = session 
-#		endtime = session2
-#		
-#hti end
-events = scraper.btlpSchedule
-#print events
-
-def get_events(existing_events = [], existing_event_endtimes = []):
-	'''
-	get list of existing events on google calendar
-	'eventlist: list stores information about each event on calendar
-	returns: list containing start times of events on calendar '''
-	now = datetime.datetime.utcnow() # 'Z' indicates UTC time
-	then= datetime.datetime.utcnow()-datetime.timedelta(7)
-	t = then.isoformat() + 'Z'
-	eventsResult = service.events().list(
+def get_events(existing_events = []):
+    '''
+    get list of existing events on google calendar
+    'eventlist: list stores information about each event on calendar
+    returns: list containing start times of events on calendar '''
+    now = datetime.datetime.utcnow() # 'Z' indicates UTC time
+    then= datetime.datetime.utcnow()-datetime.timedelta(7)
+    t = then.isoformat() + 'Z'
+    eventsResult = service.events().list(
         calendarId=cal['id'], timeMin=t, maxResults=100, singleEvents=True,
         orderBy='startTime', timeZone = 'Etc/UTC').execute()
-	events = eventsResult.get('items', [])
+    events = eventsResult.get('items', [])
 
-	if not events:
-		print('No upcoming events found.')
-	for event in events:
-		start = event['start'].get('dateTime', event['start'].get('date'))
-		end = event['end'].get('dateTime', event['start'].get('date'))
-        existing_events.append(start)
-        existing_event_endtimes.append(end)
-        
-        
-def start_time(event):
-	''' gets start time of an event scraped GBT events list'''
-	e = event.start[:10]+'T'+event.start[11:]+':00Z'
-	e = e.decode('unicode_escape')
-	return e
+    if not events:
+        print('No upcoming events found.')
+    for e in events:
+        e1 = e['summary']
+        e2 = e['start'].get('dateTime', e['start'].get('date'))
+        e3 = e['end'].get('dateTime', e['start'].get('date'))
+        e4 = e.get('htmlLink')
+        existing_events.append(Event(summary = e1, start = e2, end = e3, link  = e4 ))
 
-def end_time(event):
-	''' gets start time of an event scraped GBT events list '''
-	e = event.end[:10]+'T'+event.end[11:]+':00Z'
-	e = e.decode('unicode_escape')
-	return e
-        
-    
-def check_if_exists(event):
+def check_if_exists(scraped_event):
+	#make it so that it only checks for existing GBT events
 	''' check to see if an event from scraper is already on calendar
 	returns True if exists, False if doesn't '''
-	if start_time(event) in existing_events:
+	if start_time(scraped_event) in existing_start_times:
 		print 'Event Exists'
 		return True
-	elif end_time(event) in existing_event_endtimes:
+	elif end_time(scraped_event) in existing_end_times:
 		print 'Event Exists'
 		return True
 	else:
 		print 'Event Does Not Exist, Updating New Event'
 		return False
-    
-
-def add_event(e):
+		
+def add_event(scraped_event):
 	''' adds event to google calendar '''
-	event_start = str(start_time(e))[:-1]
-	event_end = str(end_time(e))[:-1]
+	event_start = str(start_time(scraped_event))[:-1]
+	event_end = str(end_time(scraped_event))[:-1]
 	event = {
-      'summary': 'GBT Observing '+ e.receivers +'band',
+      'summary': 'GBT Observing '+ scraped_event.receivers +'band',
       'location': '',
       'description': '',
       'start': {
@@ -127,18 +83,22 @@ def add_event(e):
       'recurrence': [
         'RRULE:FREQ=DAILY;COUNT=1'
       ],
+	'attendees': [
+    	{'email': 'isabelangelo@berkeley.edu'},
+    	{'email': 'hisaacson@berkeley.edu'},
+  	],
     }
 	event = service.events().insert(calendarId=cal['id'], body=event).execute()
 	print 'Event created: %s' % (event.get('htmlLink'))
 	print 'begin: ',event_start
 	print 'end:   ',event_end 
-
-def write_to_database(event):
+	
+def write_to_database(scraped_event):
 	'''writes the event date, time, timezone and duration to database
  	gbt_observations_database.txt '''
-	s = start_time(event)
-	a = time.strptime(str(start_time(event)), '%Y-%m-%dT%H:%M:%SZ')
-	b = time.strptime(str(end_time(event)), '%Y-%m-%dT%H:%M:%SZ')
+	s = start_time(new_event)
+	a = time.strptime(str(start_time(scraped_event)), '%Y-%m-%dT%H:%M:%SZ')
+	b = time.strptime(str(end_time(scraped_event)), '%Y-%m-%dT%H:%M:%SZ')
 	
 	a = time.mktime(a)
 	b = time.mktime(b)
@@ -157,30 +117,72 @@ def write_to_database(event):
 		file.write(entry)
 	print 'database updated'
 
+def send_text(new_event, hours):
+	'''
+	send message if event is 4 hours ahead and unclaimed:
+	'''
+	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+	client.messages.create( 
+		to = '+12134478172', 
+    	from_ = '+12138631121',
+    	body = 'Upcoming Observing Event in ' + str(hours) + ' hours\n'
+    		'claim at:'+ new_event.link,
+		)
+	print 'Observers Notified'
+		
 
 
+#get event list from GBT website
+scraper = GBTScraper(date=time.strftime("%m/%d/%Y"), days = 30) #store for next 30 days
+scraper.make(t = 'utc')
+scraped_events = scraper.btlpSchedule
 
-#get list of existing events from Google Calendar
+
+#get credentials and calendar list from Google Calendar
+credentials = get_credentials()
+http = credentials.authorize(httplib2.Http())
+service = discovery.build('calendar', 'v3', http=http)
+page_token = None
+BL_CAL = 'BL_Obs'
+while True:
+  calendar_list = service.calendarList().list(pageToken=page_token).execute()
+  for calendar_list_entry in calendar_list['items']: #hti
+    print 'calendar name:',calendar_list_entry['summary'] #hti
+    if calendar_list_entry['summary'] == BL_CAL:
+      cal = calendar_list_entry# ['items']
+      print 'credentials successful, calendar retrieved'
+      break
+  page_token = calendar_list.get('nextPageToken')
+  if not page_token:
+    break
+#cal = calendar_list['items'][2] #comment this out unless you are using the test calendar
+
+
+#store existing events on Google Calendar
 existing_events = []
-existing_event_endtimes = []
-get_events(existing_events, existing_event_endtimes)
-print existing_events
-print existing_event_endtimes
+get_events(existing_events)
+
+#store their start and end times for check)if_exists function
+existing_start_times = [] #storing existing start times
+existing_end_times = [] #storing existing end times
+for ev in existing_events:
+	#only append events with 'GBT' contained in the summary to avoid Parkes!!!
+    existing_start_times.append(ev.start)
+    existing_end_times.append(ev.end)
 
 
-#iterate over GBT events, checks if they are on the calendar, and adds if not
-# Check to see if events are consecutive
+# Merge back-to-back events before adding to calendar
 print ''
 print 'Checking for back to back events...'
 
 unq_events = []
 
-for i in range(len(events)):
+for i in range(len(scraped_events)):
 	print 'Event ',i,':',
-	event = events[i]
+	event = scraped_events[i]
 	#Reset back2back for each event
 	back2back = False
-	for other_event in events:
+	for other_event in scraped_events:
 		#this if statement extends first two events
 		if event.end == other_event.start:
 			print 'Back to back detected, events merged' 
@@ -195,7 +197,9 @@ for i in range(len(events)):
 	if back2back == False:
 		unq_events.append(event)
 		print 'Adding in single event'
+		
 
+#add unique events to calendar and write to database
 print 'Total Unique Events:', len(unq_events)
 
 print ''
@@ -207,5 +211,38 @@ for i in range(len(unq_events)):
 	if check_if_exists(event) == False:
 		add_event(event)
 		write_to_database(event)
-
 		
+
+#notify people if any events are in <4 hours
+print ''
+print 'Checking for upcoming events...'
+all_events = []
+get_events(all_events)
+upcoming_events = 0
+unclaimed_events = 0
+for ev in all_events:
+    #get event time
+    now = datetime.datetime.utcnow()
+    a = time.strptime(now.isoformat()[:-7]+'Z', '%Y-%m-%dT%H:%M:%SZ') #current time
+    b = time.strptime(ev.start, '%Y-%m-%dT%H:%M:%SZ') #time of event
+    #convert times to float
+    a = time.mktime(a) 
+    b = time.mktime(b)
+    #compute and store difference
+    d = b-a
+    if d > 0:
+    	hours = int(d) / 3600 % 24
+    	if 0 < hours < 4:
+			print 'Upcoming Event Detected in ' + str(hours) + ' hours' 
+			upcoming_events += 1
+			if ev.summary[:16] == 'Parkes Observing':
+				print 'Event Unclaimed, Observers Notified'
+				send_text(ev, hours)
+				unclaimed_events += 1
+
+if upcoming_events == 0:
+	print 'No Upcoming Events Detected'
+if unclaimed_events == 0:
+	print 'No Unclaimed Events Detected'
+
+
